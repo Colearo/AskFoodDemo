49d8ff2b5f129a385a90f815104b8055d26815a4
Colearo <lukechen@lukechendemacbook-pro.local>
2016-01-28 15:16:31 +0800

    Initial Commit

---
+++ AskFoodDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AskFoodDemo
+//
+//  Created by Colearo on 16/1/28.
+//  Copyright © 2016年 Colearo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
